9fbddfd91fcddffbd77848f95d531cbee8d5ad34
qssmqzh <ergalik2007@mail.ru>
2026-02-04 06:23:42 +0500

    Add Practice2 - Python control flow examples (boolean, if-else, loops)

---
+++ week2/boolean_values.py
@@ -0,0 +1,18 @@
+# Boolean values: True and False
+
+# Example 1
+is_raining = True
+print(is_raining)  # True
+
+# Example 2
+is_sunny = False
+print(is_sunny)  # False
+
+# Example 3
+print(type(True))  # <class 'bool'>
+
+# Example 4
+print(5 > 3)  # True
+
+# Example 5
+print(10 == 20)  # False
